701602f05bf9cb22e6e951fbc79ce1d8d003a3f8
Tommy Neubert <tommy.neubert@signavio.com>
2020-10-01 12:27:39 +0200

    replace gson with jackson

--- src/main/java/com/signavio/examples/dmn/testsuite/TestSuiteReader.java
+++ src/main/java/com/signavio/examples/dmn/testsuite/TestSuiteReader.java
@@ -2,34 +2,22 @@ package com.signavio.examples.dmn.testsuite;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.stream.JsonReader;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.signavio.bdm.testlab.exchange.TestSuite;
-import org.json.JSONObject;
 
 public class TestSuiteReader {
 	
-	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-	
-	
 	public TestSuite parse(InputStream testSuite) {
 		try {
-			JsonReader reader = createTestSuiteReader(testSuite);
-			JsonObject testSuiteJson = GSON.fromJson(reader, JsonObject.class);
-			
-			return TestSuite.fromJson(new JSONObject(testSuiteJson.toString()));
+			return new ObjectMapper()
+					.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+					.readValue(testSuite, TestSuite.class);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-	}
-	
-	
-	private JsonReader createTestSuiteReader(InputStream testSuite) {
-		return new JsonReader(new InputStreamReader(testSuite));
 	}
 	
 }
