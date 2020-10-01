6ec27db9a6d412398d4a8306247f83e8f9381d18
Jonas Beyer <jonas.beyer@signavio.com>
2020-10-01 11:51:33 +0200

    Add working own-types DMN XML example (#9)

---
+++ src/main/java/com/signavio/examples/dmn/OwnTypesDmnExample.java
@@ -0,0 +1,47 @@
+package com.signavio.examples.dmn;
+
+import java.math.BigDecimal;
+
+import com.signavio.examples.owntypes.CustomerData;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNResult;
+
+public class OwnTypesDmnExample extends AbstractDmnExample {
+	
+	private static final String DESCRIPTION = "Example using an own type";
+	
+	private static final String KNOWLEDGE_BASE_ID = "SignavioExampleDMNOwnTypesKB";
+	
+	
+	public OwnTypesDmnExample() {
+		super(KNOWLEDGE_BASE_ID);
+	}
+	
+	
+	@Override
+	public String getDescription() {
+		return DESCRIPTION;
+	}
+	
+	
+	@Override
+	public void execute() {
+		// parsing model from .dmn
+		DMNModel model = getDmnRuntime().getModels().get(0); // assuming there is only one model in the KieBase
+		
+		// setting input data
+		DMNContext dmnContext = createDmnContext(
+				ImmutablePair.of("customerData", new CustomerData("Silver", new BigDecimal(15)))
+		);
+		
+		// executing decision logic
+		DMNResult topLevelResult = getDmnRuntime().evaluateAll(model, dmnContext);
+		
+		// retrieving execution results
+		System.out.println("--- results of evaluating all ---");
+		topLevelResult.getDecisionResults().forEach(this::printAsJson);
+	}
+	
+}
